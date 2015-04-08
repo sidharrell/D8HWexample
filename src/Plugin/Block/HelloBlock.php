d959e1d0878b8b097acc9a1e88935011139f108d
Sidney Harrell <sidney.harrell@gmail.com>
2015-04-07 22:12:42 -0400

    added form to block configuration

--- src/Plugin/Block/HelloBlock.php
+++ src/Plugin/Block/HelloBlock.php
@@ -3,6 +3,7 @@ namespace Drupal\hello_world\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Block\BlockPluginInterface;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Provides a 'Hello' Block
@@ -21,5 +22,23 @@ class HelloBlock extends BlockBase implements BlockPluginInterface {
     return array(
       '#markup' => $this->t('Hello, World!'),
     );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form = parent::blockForm($form, $form_state);
+
+    $config = $this->getConfiguration();
+
+    $form['hello_block_settings'] = array (
+      '#type' => 'textfield',
+      '#title' => $this->t('Who'),
+      '#description' => $this->t('Who do you want to say hello to?'),
+      '#default_value' => isset($config['hello_block_settings']) ? $config['hello_block_settings'] : ''
+    );
+
+    return $form;
   }
 }
