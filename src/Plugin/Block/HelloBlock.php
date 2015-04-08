f00f8f6bfb6b811d1ba21bcf384ceee4dcaea8b6
Sidney Harrell <sidney.harrell@gmail.com>
2015-04-08 00:21:20 -0400

    Process the form

--- src/Plugin/Block/HelloBlock.php
+++ src/Plugin/Block/HelloBlock.php
@@ -41,4 +41,11 @@ class HelloBlock extends BlockBase implements BlockPluginInterface {
 
     return $form;
   }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->setConfigurationValue('hello_block_settings', $form_state->getValue('hello_block_settings'));
+  }
 }
